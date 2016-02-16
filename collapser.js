765de66b3c1411ff5744d5e26202f6530eb22477
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2016-02-16 15:04:24 -0200

    Convertido para plugin JQuery

--- collapser.js
+++ collapser.js
@@ -1,16 +1,13 @@
-var colapser = (function(){
-	var _colapser = function(elements, started)
-  {
-    $(elements).bind("click touchstart", function(){
-    	console.log(this);
-      var BUTTON = $(this);
-      var NxtElement = $(this).next();
-      NxtElement.stop(true, true).slideToggle();
-    });
-    
-    if(started)
-  		$(elements).trigger('click');
-  };
-  
-  return _colapser;
-})();
+(function($) {
+    $.fn.collapser = function(started) {
+      $(this).bind("click touchstart", function(){        
+        var BUTTON = $(this);
+        var NxtElement = $(this).next();
+        NxtElement.stop(true, true).slideToggle();
+      });
+      
+      if(started)
+        $(this).trigger('click');
+    };
+
+})(jQuery);
