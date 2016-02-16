2f91ebe2525808d364dc4d153826986f8e06c52a
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2016-02-16 15:10:43 -0200

    Refatorado para remover vasriáveis desnecessárias e aplicado extension para opções

--- collapser.js
+++ collapser.js
@@ -1,12 +1,18 @@
 (function($) {
-    $.fn.collapser = function(started) {
+    $.fn.collapser = function(options) {
+
+      options = $.extend({
+            opened: false
+        }, options);
+
       $(this).bind("click touchstart", function(){        
-        var BUTTON = $(this);
-        var NxtElement = $(this).next();
-        NxtElement.stop(true, true).slideToggle();
+        $(this)
+          .next()
+          .stop(true, true)
+          .slideToggle();
       });
       
-      if(started)
+      if(options.opened)
         $(this).trigger('click');
     };
 
