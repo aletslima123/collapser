a99994db0318add19152c7a7a9bfa0ecf8d1314c
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2016-02-16 15:13:52 -0200

    ajustado merge

--- collapser.js
+++ collapser.js
@@ -1,16 +1,13 @@
-var collapser = (function(){
-	var _collapser = function(elements, started)
-  {
-    $(elements).bind("click touchstart", function(){
-    	console.log(this);
-      var BUTTON = $(this);
-      var NxtElement = $(this).next();
-      NxtElement.stop(true, true).slideToggle();
-    });
-    
-    if(started)
-  		$(elements).trigger('click');
-  };
-  
-  return _collapser;
-})();
+(function($) {
+    $.fn.collapser = function(started) {
+      $(this).bind("click touchstart", function(){        
+        var BUTTON = $(this);
+        var NxtElement = $(this).next();
+        NxtElement.stop(true, true).slideToggle();
+      });
+      
+      if(started)
+        $(this).trigger('click');
+    };
+
+})(jQuery);
